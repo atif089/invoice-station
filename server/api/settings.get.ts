4fe3632ceffc4a7046a792bf2c8d36702ee46af2
Atif Mohammed <atif089@gmail.com>
2022-07-25 01:33:03 +0200

    added settings get endpoint

--- server/api/settings.get.ts
+++ server/api/settings.get.ts
@@ -1,14 +1,42 @@
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+} from 'firebase/firestore'
+import { firebaseApp } from '@/utils/firestore'
+
 type HTTPResponse = {
   status: string
   [key: string]: any
 }
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const response: HTTPResponse = {
-    status: 'success',
+    status: '',
   }
 
-  response.user = event?.context?.user || null
+  const userId = event?.context?.user?.userId
+
+  if (!userId) {
+    response.status = '401'
+    response.error = 'Cannot find user id'
+    return response
+  }
+
+  const db = getFirestore(firebaseApp)
+
+  const settingsRef = collection(db, 'users')
+  const q = query(settingsRef, where('user_id', '==', userId))
+  const querySnapshot = await getDocs(q)
+  let userData = {}
+
+  querySnapshot.forEach((doc) => {
+    userData = doc.data()
+  })
+
+  response.user = userData
 
   return response
 })
