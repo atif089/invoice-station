2c04738ab2d703f26411ba6e1790eafda7c96796
Atif Mohammed <atif089@gmail.com>
2022-07-24 19:28:33 +0200

    added get vIban routes

---
+++ server/api/wallet/[walletId]/vIban.get.ts
@@ -0,0 +1,39 @@
+import { rapydFetch, bindKeys, RapydFetchResponse } from '@/utils/rapyd'
+
+import 'dotenv/config'
+
+const accessKey = String(process.env.RAPYD_ACCESS_KEY)
+const secretKey = String(process.env.RAPYD_SECRET_KEY)
+
+bindKeys(accessKey, secretKey)
+
+type MyHTTPResponse = {
+  status: RapydFetchResponse['status'] | string
+  data?: RapydFetchResponse['data']
+  error?: string
+}
+
+export default defineEventHandler(async (event) => {
+  const walletId = event?.context?.params?.walletId
+  const response: MyHTTPResponse = { status: '' }
+
+  if (!walletId) {
+    response.status = `400`
+    response.error = `Missing walletId`
+    return response
+  }
+
+  const rapydResponse = await rapydFetch(
+    `/v1/issuing/bankaccounts/list?ewallet=${walletId}`,
+    {
+      httpMethod: `get`,
+    }
+  )
+
+  if (rapydResponse.status.status === `SUCCESS`) {
+    response.data = rapydResponse.data
+  }
+  response.status = rapydResponse.status
+
+  return response
+})
